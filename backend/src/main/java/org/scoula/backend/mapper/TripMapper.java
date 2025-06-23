18c35c6fe16e4de791c31ec976411d13567690d4
yeonhookim <rladusgn68@gmail.com>
2025-06-23 17:14:45 +0900

    feat: TripMapper 인터페이스 생성 (MyBatis 매핑용)

--- backend/src/main/java/org/scoula/backend/mapper/TripMapper.java
+++ backend/src/main/java/org/scoula/backend/mapper/TripMapper.java
@@ -1,4 +1,11 @@
 package org.scoula.backend.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.scoula.backend.dto.TripDTO;
+
+import java.util.List;
+
+@Mapper
 public interface TripMapper {
+    List<TripDTO> findTripsByMemberId(int memberId);
 }
